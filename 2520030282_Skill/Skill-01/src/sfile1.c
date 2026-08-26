21ae9e82048e25b82d21e51a68b302938897553b
hasinipanapakam-a11y <hasinipanapakam@gmail.com>
2026-08-26 22:31:28 +0530

    Update sfile1.c

--- 2520030282_Skill/Skill-01/src/sfile1.c
+++ 2520030282_Skill/Skill-01/src/sfile1.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
+int main(){
+pid_t pid=fork();
+
+if(pid<0){
+printf("Fork failed\n");
+return 1;
+}
+if(pid == 0){
+printf("Child Process\n");
+printf("Child PID: %d\n",getpid());
+printf("Parent PID: %d\n", getppid());
+printf("Executing ls command ... \n");
+execlp("ls", "ls", NULL);
+printf("exec failed\n");
+}
+else{
+printf("Parent Process\n");
+printf("Parent PID: %d\n", getpid());
+printf("Child PID: %d\n",pid);
+wait(NULL);
+printf("Child process completed\n");
+}}
