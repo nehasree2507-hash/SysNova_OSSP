2d03ad379fb427b271a7edd3200a03ababa04c69
Nehasree Padamati <nehasree2507@gmail.com>
2026-09-22 11:18:09 +0530

    Update practiclient6.c

--- 2520030174_Practical/Practical-06/src/practiclient6.c
+++ 2520030174_Practical/Practical-06/src/practiclient6.c
@@ -2,14 +2,16 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
-int main()
-{
+
+int main(){
 char message[100];
 int fd;
-fd = open("myfifo", O_RDWR);
+fd = open("requestfifo", O_WRONLY);
 printf("Enter message: ");
 fgets(message, sizeof(message), stdin);
 write(fd, message, strlen(message) + 1);
+close(fd);
+fd = open("responsefifo", O_RDONLY);
 read(fd, message, sizeof(message));
 printf("Server says: %s\n", message);
 close(fd);
